0fd9e210b62eaf7379c63cb4a878e238021a6b79
Piotrek Okoński <piotrek@okonski.org>
2012-10-21 16:20:34 +0200

    Add comments describing the logic behind the code

--- app/models/activity_observer.rb
+++ app/models/activity_observer.rb
@@ -1,6 +1,11 @@
 class ActivityObserver < ActiveRecord::Observer
   observe PublicActivity::Activity
 
+  # We are only sending the Activity's id through Pusher.
+  # Then we retrieve the actual HTML of it by sending
+  # an AJAX request from client using the supplied activity_id.
+  # This is necessary, because activity has to be
+  # rendered in client's session (we need current_user).
   def after_create(activity)
     Pusher['public_activity'].trigger('new-activity', {activity_id: activity.id})
   end
